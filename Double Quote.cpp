d448ae1c09014a81d39771bb279db6363b21391c
Ken Diani <72846676+candycrushonly@users.noreply.github.com>
2020-10-18 11:41:51 +0700

    Add files via upload

---
+++ Double Quote.cpp
@@ -0,0 +1,12 @@
+//CREATE A PROGRAM WITH OUTPUT : 
+/* I am Writing : "Hello World" while using C++ Language
+for notes this program is static program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "I am Writing : \"Hello World\" While using C++ Language"  ;
+    return 0 ;
+}
